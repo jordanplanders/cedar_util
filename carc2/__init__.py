7b2c8d41b90f4bf4d9347d01b9553b97da795394
Jordan Landers <jordan.landers+dev@gmail.com>
2025-03-20 11:38:04 -0700

    path construction moved to slurm scripts
    if this were configured to be installed as a package, would need to handle differently.

--- carc2/__init__.py
+++ carc2/__init__.py
@@ -1,7 +1,7 @@
-import sys
-import os
-# Get the directory path of the package
-
-sys.path.append('./utils')
-sys.path.append('./local2')
-sys.path.append('./ccm_utils')
+# import sys
+# import os
+# # Get the directory path of the package
+#
+# sys.path.append('./utils')
+# sys.path.append('./local2')
+# sys.path.append('./ccm_utils')
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,7 @@
-# from utils.arg_parser import get_parser
-
+# import sys
+# import os
+# # Get the directory path of the package
+#
+# sys.path.append('./utils')
+# sys.path.append('./carc2')
+# sys.path.append('./ccm_utils')
