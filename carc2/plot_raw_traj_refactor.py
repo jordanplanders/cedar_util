9e3f961c90279210db106b4d155a114b974bdffc
Jordan Landers <jordan.landers+dev@gmail.com>
2025-05-23 13:13:13 -0700

    update handling of file writing and unified override/overwrite  flags

--- carc2/plot_raw_traj_refactor.py
+++ carc2/plot_raw_traj_refactor.py
@@ -15,8 +15,11 @@ from scipy.stats import permutation_test
 from utils.arg_parser import get_parser, parse_flags
 from utils.location_helpers import construct_convergence_name
 from utils.config_parser import load_config
-from utils.data_access import get_real_data, streamline_cause, collect_raw_data, get_weighted_flag, set_df_weighted, write_query_string, relationship_filter, rel_reformat, get_surrogate_data
+from utils.data_access import get_real_output, streamline_cause, collect_raw_output, get_weighted_flag, set_df_weighted, write_query_string, relationship_filter, rel_reformat, get_surrogate_output
 from utils.location_helpers import *
+from utils.run_tools import decide_file_handling
+
+
 # from utils.data_processing import relationship_filter, rel_reformat
 #####
 # Functions to construct figures underpinnig summary grid results (seen in SI Fig 3, details)
@@ -72,10 +75,10 @@ from utils.location_helpers import *
 #
 #     return real_df
 
-# import re
-# def remove_numbers(input_string):
-#     # Use regex to remove all digits from the string
-#     return re.sub(r'\d+', '', input_string)
+import re
+def remove_numbers(input_string):
+    # Use regex to remove all digits from the string
+    return re.sub(r'\d+', '', input_string)
 
 
 # # Function to fetch and prepare surrogate data
@@ -140,8 +143,19 @@ def construct_delta_rho_df(real_df_full, surr_df_full, target_relation, conv_mat
     if 'convergence_interval' not in conv_match_d:
         print('No convergence interval found', file=sys.stderr, flush=True)
         return None
-
-    (interval_min, interval_max) = conv_match_d['convergence_interval']
+    print('construct_delta_rho_df', file=sys.stderr, flush=True)
+    if isinstance(conv_match_d['convergence_interval'], tuple) == True:
+        (interval_min, interval_max) = conv_match_d['convergence_interval']
+    else:
+        print('conv_interval?', conv_match_d['convergence_interval'], type(conv_match_d['convergence_interval']), file=sys.stderr, flush=True)
+        (interval_min, interval_max) = ast.literal_eval(conv_match_d['convergence_interval'])
+    # try:
+    #     interval_min, interval_max = conv_match_d['convergence_interval']
+    # except:
+    #     (interval_min, interval_max) = conv_match_d['convergence_interval']
+    # else:
+    #     print('Error unpacking convergence interval', file=sys.stderr, flush=True)
+    #     return None
     real_df = real_df_full[(real_df_full['LibSize'] >= interval_min) & (real_df_full['LibSize'] <= interval_max)].copy()
     surr_df = surr_df_full[(surr_df_full['LibSize'] >= interval_min) & (surr_df_full['LibSize'] <= interval_max)].copy()
 
@@ -199,6 +213,7 @@ def construct_delta_rho_df(real_df_full, surr_df_full, target_relation, conv_mat
             delta_rho_dfs.append(delta_df_tmp)
 
     delta_rho_df = pd.concat(delta_rho_dfs).reset_index(drop=True)
+    print('delta_rho_df', len(delta_rho_df), file=sys.stderr, flush=True)
     return delta_rho_df
 
 
@@ -321,10 +336,12 @@ def generate_figure_layout(has_surrogates):
         ncols = 5
         figsize = (8, 3)
         width_ratios = [1, 0.2, 0.4, 0.2, 0.4]
+        print('has_surrogates', file=sys.stderr, flush=True)
     else:
         ncols = 3
         figsize = (8, 3)
         width_ratios = [1, 0, 1]
+        print('no_surrogates', file=sys.stderr, flush=True)
 
     fig = plt.figure(figsize=figsize)
     gs = fig.add_gridspec(1, ncols, width_ratios=width_ratios, wspace=0.05)
@@ -436,10 +453,27 @@ def save_annotated_fig(fig, ax, annotations, positions, save_path_template):
         ax.texts.pop()  # Remove text for the next iteration
 
 
+def write_to_file(ccm_out_df, df_path, overwrite=False):
+    remove_cols = ['Tp_lag_total', 'sample', 'weighted', 'train_ind_0', 'run_id', 'ind_f', 'Tp_flag', 'train_len',
+                   'train_ind_i']
+    ccm_out_df = ccm_out_df[[col for col in ccm_out_df.columns if col not in remove_cols]].copy()
+
+    try:
+        ccm_out_df_0 = pd.read_csv(df_path, index_col=0)
+        if overwrite == False:
+            ccm_out_df_0 = ccm_out_df_0[[col for col in ccm_out_df_0.columns if col not in remove_cols]].copy()
+            ccm_out_df = pd.concat([ccm_out_df_0, ccm_out_df])
+            ccm_out_df.reset_index(drop=True, inplace=True)
+    except:
+        pass
+
+    ccm_out_df.to_csv(df_path)
+    print('!\twrote to file: ', df_path)
+
 ############ Main Processing Function
 def process_group_workflow(arg_tuple):
     (grp_d, ind, real_dfs_references, surr_dfs_references, conv_match_d, pctile_range,
-     override, write,  config, calc_location, output_dir, raw_fig_dir, delta_rho_dir_csv_parts, function_flag) = arg_tuple
+     args,  config, calc_location, output_dir, raw_fig_dir, delta_rho_dir_csv_parts, function_flag) = arg_tuple
     print('E', grp_d['E'], 'tau', grp_d['tau'], 'ind', ind, 'group_id', grp_d['group_id'], 'function_flag', function_flag)
     meta_variables = ['tau', 'E', 'train_len', 'train_ind_i', 'knn', 'Tp_flag',
                       'Tp', 'lag', 'Tp_lag_total', 'sample', 'weighted', 'target_var',
@@ -449,30 +483,38 @@ def process_group_workflow(arg_tuple):
 
 
     if 'convergence_interval' in conv_match_d:
-        if type(conv_match_d['convergence_interval'][0]) == str:
+        if type(conv_match_d['convergence_interval']) == str:
             try:
-                conv_match_d['convergence_interval'] = ast.literal_eval(conv_match_d['convergence_interval'])
+                conv_match_d['convergence_interval'] = ast.literal_eval(conv_match_d['convergence_interval'].replace('np.float64', ''))
             except Exception as e:
                 print(f'Error parsing convergence interval: {e}', file=sys.stderr)
             # conv_match_d['convergence_interval'] = [0, 0]
 
     max_libsize = 325
     knn = 20
-    target_relation = config.calc_convergence_reg.target_relation#get_nested_attribute('calc_criteria_rates2.target_relation')
+    target_relation = config.target_relation#get_nested_attribute('calc_criteria_rates2.target_relation')
     weighted_flag = get_weighted_flag(grp_d)
 
     range_label = '{}_{}'.format(pctile_range[0], pctile_range[1])
 
     var = 'delta_r_rho'
     fig_sub_dir = raw_fig_dir / f'{grp_d["col_var_id"]}_{grp_d["target_var_id"]}' / var
+    # fig_sub_dir.mkdir(exist_ok=True, parents=True)
     fig_target_path = fig_sub_dir / f'{grp_d["col_var_id"]}_{grp_d["target_var_id"]}_E{str(grp_d["E"])}_tau{grp_d["tau"]}_{weighted_flag}_Tp{grp_d["Tp"]}_startind{grp_d["train_ind_i"]}_knn{grp_d["knn"]}_lag{grp_d["lag"]}_{grp_d["group_id"]}.pdf'
-    if fig_target_path.exists() and override is False:
-        print(f'Skipped processing, file exists: {fig_target_path}', file=sys.stdout, flush=True)
+    # if fig_target_path.exists()==True:
+    #     if override == False:
+    #         print(f'Skipped processing, file exists: {fig_target_path}', file=sys.stdout, flush=True)
+    #         return None
+    fig_existence = fig_target_path.exists()
+    run_continue, overwrite = decide_file_handling(args, fig_existence)
+    if run_continue == False:
+        print(f'\Skipped processing, file exists: {fig_target_path}', file=sys.stdout, flush=True)
         return None
 
+    print('fig_target_path', fig_target_path, file=sys.stdout, flush=True)
     ik = 0
     while ik < 10:
-        real_df_full = get_real_data(real_dfs_references, grp_path, meta_variables, max_libsize, knn)
+        real_df_full = get_real_output(real_dfs_references, grp_path, meta_variables, max_libsize, knn)
         if real_df_full is None:
             print('No real data found', grp_d, file=sys.stderr, flush=True)
             if ik == 9:
@@ -481,10 +523,12 @@ def process_group_workflow(arg_tuple):
                 ik += 1
         else:
             ik = 10
+    print('real_df_full', len(real_df_full), file=sys.stderr, flush=True)
 
     ik = 0
+    print('surr_dfs_references', surr_dfs_references, file=sys.stdout, flush=True)
     while ik<10:
-        surr_df_full = get_surrogate_data(surr_dfs_references, grp_path, meta_variables, max_libsize, knn, target_relation)
+        surr_df_full = get_surrogate_output(surr_dfs_references, grp_path, meta_variables, max_libsize, knn, target_relation)
         if surr_df_full is None:
             print('No surrogate data found', grp_d, file=sys.stderr, flush=True)
             if ik == 9:
@@ -493,6 +537,7 @@ def process_group_workflow(arg_tuple):
                 ik +=1
         else:
             ik = 10
+    print('surr_df_full', len(surr_df_full), file=sys.stderr, flush=True)
 
     ik = 0
     while ik < 10:
@@ -507,25 +552,35 @@ def process_group_workflow(arg_tuple):
 
 
     if delta_rho_df is not None and not delta_rho_df.empty:
-        delta_rho_df['range_label'] = range_label
-        test_results = perform_statistical_tests(delta_rho_df, var_1='rho_r', var_2='rho_s')
-        test_results += perform_statistical_tests(delta_rho_df, var_1='rho_r', var_2='min_rho')
-        test_results += perform_statistical_tests(delta_rho_df, var_1='final_rho', var_2='min_rho')
-        test_results += perform_statistical_tests(delta_rho_df, var_1='final_rho_top', var_2='min_rho_top')
-
-        test_results = pd.DataFrame(test_results)
-
         csv_path = delta_rho_dir_csv_parts / f'delta_rho_grp{grp_d["group_id"]}_ind{ind}.csv'
-        if write=='replace' or not csv_path.exists():
-            test_results.to_csv(csv_path, index=False)
-            print(f'Results saved to {csv_path}', file=sys.stdout, flush=True)
+        existence = csv_path.exists()
+        run_continue, overwrite = decide_file_handling(args, existence)
+        if run_continue == False:
+            print(f'Skipped processing, file exists: {csv_path}', file=sys.stdout, flush=True)
         else:
-            print(f'Skipped saving, file exists: {csv_path}', file=sys.stdout, flush=True)
+            delta_rho_df['range_label'] = range_label
+            test_results = perform_statistical_tests(delta_rho_df, var_1='rho_r', var_2='rho_s')
+            test_results += perform_statistical_tests(delta_rho_df, var_1='rho_r', var_2='min_rho')
+            test_results += perform_statistical_tests(delta_rho_df, var_1='final_rho', var_2='min_rho')
+            test_results += perform_statistical_tests(delta_rho_df, var_1='final_rho_top', var_2='min_rho_top')
+
+            test_results = pd.DataFrame(test_results)
+            write_to_file(test_results, csv_path, overwrite=overwrite)
+            print(f'Results saved to {csv_path}', file=sys.stdout, flush=True)
+        #
+        #
+        #
+        # if write=='replace' or not csv_path.exists():
+        #     test_results.to_csv(csv_path, index=False)
+        #     print(f'Results saved to {csv_path}', file=sys.stdout, flush=True)
+        # else:
+        #     print(f'Skipped saving, file exists: {csv_path}', file=sys.stdout, flush=True)
 
         # Plotting phase
         palette = config.pal.to_dict()
 
         for var in ['delta_r_rho', 'delta_r_rho_final']:
+            print('var', var, file=sys.stderr, flush=True)
             fig, axs = generate_figure_layout(has_surrogates=not surr_df_full.empty)
 
             primary_df = pd.concat([real_df_full, surr_df_full.rename(columns={'relation':'relation2', 'relation_s':'relation'}).drop(columns=['relation2'])])
@@ -534,6 +589,7 @@ def process_group_workflow(arg_tuple):
             plot_density(axs[2], rel_reformat(rel_reformat(delta_rho_df, 'relation'), 'relation_s') , palette, var=var)
             if not surr_df_full.empty:
                 plot_comparison_density(axs[-1], rel_reformat(rel_reformat(delta_rho_df, 'relation'), 'relation_s'), palette)
+                print('plot_comparison_density', file=sys.stderr, flush=True)
 
             add_testing_interval_annotation(axs[0], conv_match_d['convergence_interval'][0],
                                             conv_match_d['convergence_interval'][1],
@@ -551,6 +607,7 @@ def process_group_workflow(arg_tuple):
 
             plt.savefig(fig_target_path, bbox_inches='tight')
             print(f'Figure saved to {fig_target_path}', file=sys.stdout, flush=True)
+            # print(os.listdir(str(fig_sub_dir)), file=sys.stdout, flush=True)
     else:
         print('No delta rho data found, exiting failed', grp_d, file=sys.stderr, flush=True)
 
@@ -569,11 +626,11 @@ if __name__ == '__main__':
         print('project name is required', file=sys.stderr, flush=True)
         sys.exit(0)
 
-    override = args.override  # if args.override not in [None else False
-    write_flag = args.write
-    if override is False:
-        if write_flag in ['append', None]:
-            write_flag = 'append'
+    # override = args.override  # if args.override not in [None else False
+    # write_flag = args.write
+    # if override is False:
+    #     if write_flag in ['append', None]:
+    #         write_flag = 'append'
 
     second_suffix = ''
 
@@ -622,6 +679,7 @@ if __name__ == '__main__':
     # Create directories for saving figures & summary statistics
     raw_fig_dir = calc_convergence_dir / (config.calc_carc_dir.dirs.ccm_surr_plots_dir_raw + second_suffix)
     raw_fig_dir.mkdir(exist_ok=True, parents=True)
+    print('raw_fig_dir', raw_fig_dir, file=sys.stdout, flush=True)
 
     delta_rho_dir = calc_convergence_dir / (config.calc_convergence_dir.dirs.delta_rho + second_suffix)
     delta_rho_dir.mkdir(exist_ok=True, parents=True)
@@ -673,7 +731,7 @@ if __name__ == '__main__':
                     surr_dfs_references = [file for file in files if 'neither' not in file]
 
                     arg_tuples.append((grp_d, ind, real_dfs_references, surr_dfs_references, conv_match_d, pctile_range,
-                                       override, write_flag, config, calc_location,output_dir,
+                                       args, config, calc_location,output_dir,
                                        raw_fig_dir, delta_rho_dir_csv_parts, function_flag))
             else:
                 conv_match_d = {}
@@ -699,7 +757,7 @@ if __name__ == '__main__':
         else:
             grp_d = grp_ds[index]
 
-        grp_path = calc_location / 'calc_refactor' / f'{grp_d["col_var_id"]}_{grp_d["target_var_id"]}' / f'E{grp_d["E"]}_tau{grp_d["tau"]}'
+        grp_path = output_dir / f'{grp_d["col_var_id"]}_{grp_d["target_var_id"]}' / f'E{grp_d["E"]}_tau{grp_d["tau"]}'
         files = [file for file in os.listdir(grp_path) if (file.endswith('.csv'))]
         real_dfs_references = [file for file in files if 'neither' in file]
         surr_dfs_references = [file for file in files if 'neither' not in file]
@@ -708,8 +766,7 @@ if __name__ == '__main__':
         if len(conv_match) > 0:
             conv_match_d = conv_match.iloc[0].to_dict()
             print(conv_match_d, file=sys.stdout, flush=True)
-            arg_tuple = (grp_d, index, real_dfs_references, surr_dfs_references, conv_match_d, pctile_range, override,
-                         write_flag, config, calc_location,output_dir,
+            arg_tuple = (grp_d, index, real_dfs_references, surr_dfs_references, conv_match_d, pctile_range, args, config, calc_location,output_dir,
                          raw_fig_dir, delta_rho_dir_csv_parts, function_flag)
 
             process_group_workflow(arg_tuple)
