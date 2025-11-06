cdd8883bfd82ad499fc16988a4cf8ac5072bf5ae
Jordan Landers <jordan.landers+dev@gmail.com>
2025-11-05 19:36:13 -0800

    Refactor location_helpers.py and clean up unused code
    Introduced check_location to determine environment, refactored set_calc_path and set_output_path to use dynamic config attributes, and removed unused or redundant functions and commented code for clarity and maintainability.

--- utils/location_helpers.py
+++ utils/location_helpers.py
@@ -2,6 +2,20 @@
 from pathlib import Path
 import os
 import sys
+import types
+import collections.abc
+import copy
+
+
+def check_location(target_path=None, local_word='jlanders'):
+    if target_path is None:
+        target_path = Path.cwd()
+
+    if local_word in str(target_path):
+        return 'local'
+    else:
+        return 'hpc'
+
 
 def set_calc_path(args, proj_dir, config, second_suffix=''):
     calc_location = None
@@ -10,10 +24,10 @@ def set_calc_path(args, proj_dir, config, second_suffix=''):
             calc_location = proj_dir/args.calc_dir
 
     if calc_location is None:
-        if Path('/Users/jlanders').exists() == True:
-            calc_location = proj_dir / (config.local.calc_carc + f'{second_suffix}')  #'calc_local_tmp'
-        else:
-            calc_location = proj_dir / (config.local.calc_carc + f'{second_suffix}')
+        loc = check_location(proj_dir)
+        calc_dir = config.get_dynamic_attr("{var}.calc_dir", loc)
+        calc_location = proj_dir / (calc_dir + f'{second_suffix}')  #'calc_local_tmp'
+
 
     return calc_location
 
@@ -25,14 +39,10 @@ def set_output_path(args, calc_location, config):
 
     if output_dir is None:
         try:
-            # if config.carc.output_dir is not None:
-            output_dir = calc_location / config.carc.output_dir
+            output_dir = calc_location / config.hpc.output_dir
         except AttributeError:
-            print('AttributeError: config.carc.output_dir not found', file=sys.stdout, flush=True)
-            # output_dir = calc_location / 'calc_refactor'
+            print('AttributeError: config.hpc.output_dir not found', file=sys.stdout, flush=True)
 
-        # if output_dir.exists() == False:
-        #     output_dir = calc_location
 
     return output_dir
 
@@ -40,9 +50,7 @@ def replace(template, d):
     for key, value in d.items():
         template = template.replace(f'{{{key}}}', str(value))
     return template
-import types
-import collections.abc
-import copy
+
 def template_replace(template, d, return_replaced=True):
     replaced = []
     old_template = copy.copy(template)
@@ -56,33 +64,13 @@ def template_replace(template, d, return_replaced=True):
 
     return template, replaced
 
-def correct_iterable(obj):
-    if obj is None:
-        return None
-    if isinstance(obj, str):
-        return [obj]
-    else:
-        if isinstance(obj, collections.abc.Iterable):
-            return obj
-        else:
-            return [obj]
-
-def check_csv_ext(output_file_name):
-    if '.csv' not in output_file_name:
-        output_file_name = f'{output_file_name}.csv'
-    return output_file_name
-
 def set_grp_path(parent_path, d, config=None, source='csv', grp_level='grp_dir_structure', make_grp=True):
-    # print('source', source, file=sys.stdout, flush=True)
     if 'tp' in d.keys():
         d['Tp'] = d['tp']
-    # print('tried to set grp path?')
-    # print('set_grp_path', source)#, file=sys.stdout, flush=True)
     tmp_d = d.copy()
     tmp_d= {k:v[0] if isinstance(v, list) and len(v)==1 else v for k,v in tmp_d.items()}
     config_path = parent_path#set_model_config_path(parent_path, d, config=config)
 
-    # print('set_grp_path d', d, source, file=sys.stdout, flush=True)
     if source == 'csv':
         if 'lag' in d and d['lag'] is not None:
             grp_level = 'dir_structure_csv'
@@ -92,25 +80,18 @@ def set_grp_path(parent_path, d, config=None, source='csv', grp_level='grp_dir_s
     else:
         grp_level = 'dir_structure'
 
-
-    # print('grp_level', grp_level)#, file=sys.stdout, flush=True)
-
     if config is not None:
         try:
             grp_path_template = config.get_dynamic_attr("output.{var}", grp_level) # config.output.grp_dir_structure
             grp_path_template_filled = template_replace(grp_path_template,tmp_d, return_replaced=False)
-            # grp_path_template_filled = '/'.join([path_part for path_part in grp_path_template_filled.split('/') if '{' not in path_part])# grp_path_template.replace('{col_var_id}', d["col_var_id"]).replace('{target_var_id}', d["target_var_id"]).replace('{E}', str(d["E"])).replace('{tau}', str(d["tau"])).replace('{knn}', 'knn'+str(d["knn"])).replace('{Tp}', str(d["Tp"])).replace('{lag}', str(d.get("lag",0)))
             grp_path = config_path / grp_path_template_filled
-            # print('grp_path_template', grp_path_template, 'filled', grp_path_template_filled, '-> grp_path', grp_path, file=sys.stdout, flush=True)
-            # if make_grp is True:
-            #     grp_path.mkdir(exist_ok=True, parents=True)
+
         except:
             pass
     else:
         grp_path_template_filled = f'{d["col_var_id"]}_{d["target_var_id"]} / E{d["E"]}_tau{d["tau"]}'
         grp_path = config_path / grp_path_template_filled
 
-    # print('set_grp_path d', grp_path, file=sys.stdout, flush=True)
 
     if make_grp is True:
         grp_path.mkdir(exist_ok=True, parents=True)
@@ -130,22 +111,6 @@ def set_proj_dir(proj_name, current_path):
         print('proj_config.yaml not found in project directory', file=sys.stdout, flush=True)
         print('proj_config.yaml not found in project directory', file=sys.stderr, flush=True)
         sys.exit(0)
-
-
-def construct_convergence_name(args, carc_config_d, percent_threshold, second_suffix):
-    percent_threshold_label = str(percent_threshold * 100).lstrip('.0').replace('.', 'p')
-    if '.' in percent_threshold_label:
-        percent_threshold_label = '_' + percent_threshold_label.replace('.', 'p')
-
-    calc_convergence_dir_name = f'{carc_config_d.dirs.calc_convergence_dir}/tolerance{percent_threshold_label}'
-    if isinstance(args.dir, str):
-        if len(args.dir) > 0:
-            subdir = args.dir
-            calc_convergence_dir_name = f'{calc_convergence_dir_name}/{subdir}{second_suffix}'
-    else:
-        calc_convergence_dir_name = f'{calc_convergence_dir_name}{second_suffix}'
-
-    return calc_convergence_dir_name
 
 
 def check_exists(file_name, calc_dir):
@@ -169,56 +134,12 @@ def check_exists(file_name, calc_dir):
     return pset_exists, stem_exists
 
 def set_model_config_path(parent_path, d, config=None):
-    # if "lag" not in d:
-    #     d["lag"] = 0
-    # if isinstance(d["lag"], str):
-    #     if d["lag"].isdigit():
-    #         d["lag"] = int(d["lag"])
-    #     else:
-    #         d["lag"] = 0
 
     if config is not None:
         try:
             dir_structure = config.output.non_grp_structure
-            # print('dir_structure', dir_structure, file=sys.stdout, flush=True)
             dir_structure_filled = replace(dir_structure, d)
-            # print('dir_structure_filled', parent_path / dir_structure_filled, file=sys.stdout, flush=True)
             return parent_path / dir_structure_filled
         except:
             pass
     return parent_path / f'knn_{d["knn"]}' / f'tp_{d["Tp"]}' / f'lag_{d["lag"]}'
-
-def set_convergence_paths(calc_convergence_dir_parent, config, d):
-
-    calc_convergence_dir = set_model_config_path(calc_convergence_dir_parent, d)
-    calc_convergence_dir.mkdir(exist_ok=True, parents=True)
-
-    calc_convergence_dir_csvs = calc_convergence_dir / config.calc_convergence_dir.dirs.csvs
-    calc_convergence_dir_csvs.mkdir(exist_ok=True, parents=True)
-
-    convergence_dir_csv_parts = calc_convergence_dir / f'{config.calc_convergence_dir.dirs.summary_frags}'
-    convergence_dir_csv_parts.mkdir(exist_ok=True, parents=True)
-
-    fig_dir = calc_convergence_dir / f'{config.calc_convergence_dir.dirs.figures}'
-    fig_dir.mkdir(exist_ok=True, parents=True)
-
-    return calc_convergence_dir, calc_convergence_dir_csvs, convergence_dir_csv_parts, fig_dir
-
-
-def set_delta_rho_paths(config, calc_convergence_dir, second_suffix=''):
-
-    delta_rho_dir = calc_convergence_dir / (config.calc_convergence_dir.dirs.delta_rho + second_suffix)
-    delta_rho_dir.mkdir(exist_ok=True, parents=True)
-
-    delta_rho_dir_csv_parts = delta_rho_dir / config.delta_rho_dir.dirs.summary_frags
-    delta_rho_dir_csv_parts.mkdir(exist_ok=True, parents=True)
-
-    return delta_rho_dir, delta_rho_dir_csv_parts
-
-
-def set_raw_fig_dir(config, calc_convergence_dir, second_suffix=''):
-    raw_fig_dir = calc_convergence_dir / (config.calc_carc_dir.dirs.ccm_surr_plots_dir_raw + second_suffix)
-    raw_fig_dir.mkdir(exist_ok=True, parents=True)
-    print('raw_fig_dir', raw_fig_dir, file=sys.stdout, flush=True)
-
-    return raw_fig_dir
