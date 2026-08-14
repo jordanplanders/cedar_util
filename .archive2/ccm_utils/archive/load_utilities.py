59adcaf228a866413eee7fcd36b5f1a89cd20d87
Jordan Landers <jordan.landers+dev@gmail.com>
2026-08-14 16:39:16 -0700

    Remove legacy .archive2 code snapshot
    Delete the entire `.archive2` tree, including old CARC scripts, utility modules, plotting/data-object code, and archived environment/config files. This cleans out deprecated archival code that is no longer part of the active code path, reducing repository clutter and maintenance overhead.

--- .archive2/ccm_utils/archive/load_utilities.py
+++
@@ -1,6 +0,0 @@
-time = model_sol.time  #solution.t#['t_arr']
-model_sol_y = model_sol.state_variables['v']  #model_sol.solution.y[:,0]
-model_forcing = model_sol.diagnostic_variables['insolation']
-print(len(time), len(model_forcing), len(model_sol_y))
-
-df = pd.DataFrame({'time':time+length, 'ice_volume': model_sol_y, 'insolation':model_forcing})
